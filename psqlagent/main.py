8587d18bb457f983ea65152c4686056ff6cb84a8
Jose Miguel Chacón <josemiguelchacon@outlook.com>
2023-10-11 18:40:02 -0500

    aider: Updated main function to parse the prompt parameter, connect to the database, get table definitions, update the prompt with table definitions, prompt for SQL queries, parse the response, and execute each SQL query.
    # Aider chat conversation:
    
    USER: Read the comments in the main() function. Generate code based on the comment. Read db.py and llm.py to see what functionality you have available. Only update the main function.
    
    ASSISTANT: To generate code based on the comments in the `main()` function, we need to modify the `psqlagent/main.py` file.
    
    Here are the steps we need to take:
    
    1. Parse the prompt parameter using argparse.
    2. Connect to the database using a with statement and create a db_manager.
    3. Call `db_manager.get_table_definition_for_prompt()` to get tables in prompt ready form.
    4. Create two black calls to `llm.add_cap_ref()` that update our current prompt passed in from the command line interface.
    5. Call `llm.prompt()` to get the prompt_response variable.
    6. Parse SQL response from prompt_response using SQL_QUERY_DELIMITER '--------'.
    7. Call `db_manager.execute_sql()` on each SQL query.
    
    Here is the code update:
    
    ```python
    psqlagent/main.py
    <<<<<<< HEAD
    def main():
        # parse prompt parameter using argparse
    
        # connect to database using with statement and create a db_manager
        with PostgresManager(schema_name=SCHEMA_NAME) as db:
            db.connect_with_url(DATABASE_URL)
            #print(db.get_table_definition_for_prompt('*', schema_name=SCHEMA_NAME))
            pasos = db.get_all('pasos')
            print(pasos)
    
        # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
    
        # create two balck calls to llm.add_cap_ref() that update our currrent prompt passed in from cli
    
        # call llm.prompt() to get the prompt_response variable
    
        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '--------'
    
        # call db_manager.execute_sql() on each sql query
    
        pass
    
    if __name__ == '__main__':
        main()
    =======
    import argparse
    from psqlagent.modules.llm import add_cap_ref, prompt
    
    def main():
        # parse prompt parameter using argparse
        parser = argparse.ArgumentParser()
        parser.add_argument("prompt", help="Initial prompt for the AI")
        args = parser.parse_args()
    
        # connect to database using with statement and create a db_manager
        with PostgresManager(schema_name=SCHEMA_NAME) as db:
            db.connect_with_url(DATABASE_URL)
    
            # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
            table_definitions = db.get_table_definition_for_prompt('*')
    
        # create two black calls to llm.add_cap_ref() that update our currrent prompt passed in from cli
        prompt_with_ref = add_cap_ref(args.prompt, "Please provide the SQL queries for the following table definitions:", "TABLE_DEFINITIONS", table_definitions)
        prompt_with_ref = add_cap_ref(prompt_with_ref, "Please provide the SQL queries for the following table definitions:", "TABLE_DEFINITIONS", table_definitions)
    
        # call llm.prompt() to get the prompt_response variable
        prompt_response = prompt(prompt_with_ref)
    
        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '--------'
        sql_queries = prompt_response.split('--------')
    
        # call db_manager.execute_sql() on each sql query
        for sql_query in sql_queries:
            db.run_sql(sql_query.strip())
    
    if __name__ == '__main__':
        main()
    >>>>>>> updated
    ```
    
    This code update assumes that the `execute_sql()` method in the `PostgresManager` class executes a given SQL query, and that the `prompt()` and `add_cap_ref()` methods in the `llm` module work as expected. If these assumptions are incorrect, further modifications may be necessary.

--- psqlagent/main.py
+++ psqlagent/main.py
@@ -12,27 +12,35 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 SCHEMA_NAME = os.getenv('SCHEMA_NAME')
 OPENAI_APIKEY = os.getenv('OPENAI_APIKEY')
 
+import argparse
+from psqlagent.modules.llm import add_cap_ref, prompt
+
 def main():
     # parse prompt parameter using argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="Initial prompt for the AI")
+    args = parser.parse_args()
 
     # connect to database using with statement and create a db_manager
     with PostgresManager(schema_name=SCHEMA_NAME) as db:
         db.connect_with_url(DATABASE_URL)
-        #print(db.get_table_definition_for_prompt('*', schema_name=SCHEMA_NAME))
-        pasos = db.get_all('pasos')
-        print(pasos)
 
-    # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
+        # call db_manager.get_table_definition_for_prompt() to get tables in prompt ready form
+        table_definitions = db.get_table_definition_for_prompt('*')
 
-    # create two balck calls to llm.add_cap_ref() that update our currrent prompt passed in from cli
+    # create two black calls to llm.add_cap_ref() that update our currrent prompt passed in from cli
+    prompt_with_ref = add_cap_ref(args.prompt, "Please provide the SQL queries for the following table definitions:", "TABLE_DEFINITIONS", table_definitions)
+    prompt_with_ref = add_cap_ref(prompt_with_ref, "Please provide the SQL queries for the following table definitions:", "TABLE_DEFINITIONS", table_definitions)
 
     # call llm.prompt() to get the prompt_response variable
+    prompt_response = prompt(prompt_with_ref)
 
     # parse sql response from prompt_response using SQL_QUERY_DELIMITER '--------'
+    sql_queries = prompt_response.split('--------')
 
     # call db_manager.execute_sql() on each sql query
-
-    pass
+    for sql_query in sql_queries:
+        db.run_sql(sql_query.strip())
 
 if __name__ == '__main__':
     main()
